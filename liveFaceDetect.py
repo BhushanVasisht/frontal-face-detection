77eb6e31d4adfbfe42c49d3cc55329fb12aa7c13
MangiriRanga <38139437+MangiriRanga@users.noreply.github.com>
2018-04-09 03:21:45 +0530

    Update liveFaceDetect.py

--- liveFaceDetect.py
+++ liveFaceDetect.py
@@ -1,13 +1,6 @@
 import cv2
 
-def detect_faces(blur_frame, frame):
-    faces = face_cascade.detectMultiScale(blur_frame, scaleFactor=1.2, minNeighbors=5)
-
-    print(faces)
-
-    for x,y,w,h in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,255), 3)
-    return frame
+import face_model
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier("D:/UDEMY/Python Mega Course - 11 Projects/18 Python for Image and Video Processing with OpenCV/170 Files/haarcascade_frontalface_default.xml")
