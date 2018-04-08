88250e5482b31a42652ad291664e0d40c519d7ec
MangiriRanga <38139437+MangiriRanga@users.noreply.github.com>
2018-04-09 03:27:44 +0530

    Face Model Library

--- face_model.py
+++ face_model.py
@@ -6,5 +6,5 @@ def detect_faces(blur_frame, frame):
     print(faces)
 
     for x,y,w,h in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,255), 3)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,250), 3)
     return frame
