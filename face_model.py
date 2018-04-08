06d739243ce84f0ea4355513b85b419f3c6d2355
MangiriRanga <38139437+MangiriRanga@users.noreply.github.com>
2018-04-09 03:21:23 +0530

    Create face_model.py

---
+++ face_model.py
@@ -0,0 +1,10 @@
+import cv2
+
+def detect_faces(blur_frame, frame):
+    faces = face_cascade.detectMultiScale(blur_frame, scaleFactor=1.2, minNeighbors=5)
+
+    print(faces)
+
+    for x,y,w,h in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,255), 3)
+    return frame
